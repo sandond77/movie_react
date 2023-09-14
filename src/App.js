b2a1739a11cdadee122b836ce9e936d36887e589
unknown <sandu@ucdavis.edu>
2023-09-14 00:19:15 -0700

    add given CSS file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export default function App() {
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=449c886e';
+
+export default function App() { 
+  const searchMovies = async (title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(()=>{
+    searchMovies('Spiderman');
+  },[]);
+
   return (
     <div>App</div>
   )
