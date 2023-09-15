2d561973946da40da28360d4e30f4f1c5dc0d922
unknown <sandu@ucdavis.edu>
2023-09-14 22:46:29 -0700

    Add search bar typing functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=449c886e';
 
 export default function App() { 
   const [movies, setMovies]  = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const searchMovies = async (title)=>{
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
@@ -33,8 +34,8 @@ export default function App() {
       <div className='search'>
         <input 
           placeholder='Search for movies'
-          value="superman"
-          onChange={()=>{}}
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)}
         />
         <img
           src={searchIcon}
@@ -45,13 +46,13 @@ export default function App() {
 
       {
         movies.length > 0 
-          ?(
+          ? (
             <div className='container'>
               {movies.map((movie) => 
                 <MovieCard movie={movie} />
               )}
             </div>
-          ):(
+          ) : (
             <div className='empty'>
               <h2>No Movies Found!</h2>
             </div>
