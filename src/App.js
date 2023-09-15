195723358a10dce56f0eecc024e3815e9112c49a
unknown <sandu@ucdavis.edu>
2023-09-14 22:43:47 -0700

    MovieCard generated through the object mapping

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import searchIcon from './search.svg';
 import MovieCard from './MovieCard';
@@ -6,20 +6,21 @@ import MovieCard from './MovieCard';
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=449c886e';
 
 export default function App() { 
+  const [movies, setMovies]  = useState([]);
   const searchMovies = async (title)=>{
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
-  const movie1 = {
-    "Title": "Superman, Spiderman or Batman",
-    "Year": "2011",
-    "imdbID": "tt2084949",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-  };
+  // const movie1 = {
+  //   "Title": "Superman, Spiderman or Batman",
+  //   "Year": "2011",
+  //   "imdbID": "tt2084949",
+  //   "Type": "movie",
+  //   "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+  // };
 
   useEffect(()=>{
     searchMovies('Spiderman');
@@ -42,9 +43,22 @@ export default function App() {
         />
       </div>
 
-      <div className='container'>
-        <MovieCard movie1={movie1}/>
-      </div>
+      {
+        movies.length > 0 
+          ?(
+            <div className='container'>
+              {movies.map((movie) => 
+                <MovieCard movie={movie} />
+              )}
+            </div>
+          ):(
+            <div className='empty'>
+              <h2>No Movies Found!</h2>
+            </div>
+          )
+      }
+
+
     </div>
   )
 }
